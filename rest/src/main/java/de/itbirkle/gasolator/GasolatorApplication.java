29594e552670bb8695ec53daf13cb61b1f81e359
patrick-birkle <internet@birkle.email>
2020-10-24 12:57:43 +0200

    initialize rest service

---
+++ rest/src/main/java/de/itbirkle/gasolator/GasolatorApplication.java
@@ -0,0 +1,13 @@
+package de.itbirkle.gasolator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasolatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GasolatorApplication.class, args);
+    }
+
+}
---
+++ rest/src/test/java/de/itbirkle/gasolator/GasolatorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.itbirkle.gasolator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasolatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
